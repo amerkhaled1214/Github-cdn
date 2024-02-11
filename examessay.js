d33c06cb3e15207e9e8422909e86b19933354e24
amerkhaled1214 <69303247+amerkhaled1214@users.noreply.github.com>
2024-02-11 16:42:06 +0200

    Add files via upload

---
+++ examessay.js
@@ -0,0 +1,202 @@
+const MainScrptQues = [
+  {
+    id: 1,
+    q: "Frist Scenario: Assume fixed costs are allocated based on the proportion of units produced by each center. What is Center D’s per unit cost?",
+    options: [],
+    answer: 4,
+    notID: 1,
+    yourAns: "",
+    yourNote: "",
+    yourChck: "",
+    id2: 1,
+    timeValues: "https://drive.google.com/file/d/1Pub1iP9byprNvqcSSxS_b2L3RSoiD8AN/view?usp=sharing",
+    essayScenario: "https://drive.google.com/file/d/1fbv59XWaxdd0jHovGJbkHeZx3eKPuxjw/view?usp=sharing",
+    ansExplain:
+      '<div class="ExplnAns">Correct answer D.</div>',
+  },
+  {
+  id: 2,
+  q: "Frist Scenario: What would be Center A’s per unit cost if Center E’s fixed costs are allocated based on practical capacity?",
+  options: [],
+  answer: 4,
+  notID: 2,
+  yourAns: "",
+  yourNote: "",
+  yourChck: "",
+  id2: 1,
+  timeValues: "https://drive.google.com/file/d/1Pub1iP9byprNvqcSSxS_b2L3RSoiD8AN/view?usp=sharing",
+  essayScenario: "https://drive.google.com/file/d/1fbv59XWaxdd0jHovGJbkHeZx3eKPuxjw/view?usp=sharing",
+  ansExplain:
+    '<div class="ExplnAns">Correct answer D.</div>',
+  },
+  {
+    id: 3,
+    q: `Frist Scenario: Although allocating Center E’s fixed costs on a per-unit produced basis seems equitable, the manager of Center C is worried about Center B reducing the number of units produced by 2,500 units. Calculate Center C’s per unit cost with no change in production.`,
+    options: [],
+    answer: 4,
+    notID: 3,
+    yourAns: "",
+    yourNote: "",
+    yourChck: "",
+    id2: 1,
+    timeValues: "https://drive.google.com/file/d/1Pub1iP9byprNvqcSSxS_b2L3RSoiD8AN/view?usp=sharing",
+    essayScenario: "https://drive.google.com/file/d/1fbv59XWaxdd0jHovGJbkHeZx3eKPuxjw/view?usp=sharing",
+    ansExplain:
+      '<div class="ExplnAns">Correct answer D.</div>',
+    },
+    {
+      id: 4,
+      q: "Frist Scenario: If Center B reduces the number of units produced to 5,000, will Center C’s cost increase or decrease and by how much?",
+      options: [],
+      answer: 4,
+      notID: 4,
+      yourAns: "",
+      yourNote: "",
+      yourChck: "",
+      id2: 1,
+      timeValues: "https://drive.google.com/file/d/1Pub1iP9byprNvqcSSxS_b2L3RSoiD8AN/view?usp=sharing",
+      essayScenario: "https://drive.google.com/file/d/1fbv59XWaxdd0jHovGJbkHeZx3eKPuxjw/view?usp=sharing",
+      ansExplain:
+        '<div class="ExplnAns">Correct answer D.</div>',
+    },
+    {
+        id: 5,
+        q: "Frist Scenario: The center managers are concerned that being charged for unused capacity will impact their bonus.Explain how company management could alleviate the concerns.",
+        options: [],
+        answer: 4,
+        notID: 5,
+        yourAns: "",
+        yourNote: "",
+        yourChck: "",
+        id2: 1,
+        timeValues: "https://drive.google.com/file/d/1Pub1iP9byprNvqcSSxS_b2L3RSoiD8AN/view?usp=sharing",
+        essayScenario: "https://drive.google.com/file/d/1fbv59XWaxdd0jHovGJbkHeZx3eKPuxjw/view?usp=sharing",
+        ansExplain:
+          '<div class="ExplnAns">Correct answer D.</div>',
+    },
+    {
+      id: 6,
+      q: "Frist Scenario: Identify three additional measures that could be used to evaluate manager performance",
+      options: [],
+      answer: 4,
+      notID: 5,
+      yourAns: "",
+      yourNote: "",
+      yourChck: "",
+      id2: 1,
+      timeValues: "https://drive.google.com/file/d/1Pub1iP9byprNvqcSSxS_b2L3RSoiD8AN/view?usp=sharing",
+      essayScenario: "https://drive.google.com/file/d/1fbv59XWaxdd0jHovGJbkHeZx3eKPuxjw/view?usp=sharing",
+      ansExplain:
+        '<div class="ExplnAns">Correct answer D.</div>',
+  },
+  {
+    id: 7,
+    q: "Second Scenario: Identify and explain four characteristics that could make this company’s budgeting process successful.",
+    options: [],
+    answer: 4,
+    notID: 1,
+    yourAns: "",
+    yourNote: "",
+    yourChck: "",
+    id2: 1,
+    timeValues: "https://drive.google.com/file/d/1Pub1iP9byprNvqcSSxS_b2L3RSoiD8AN/view?usp=sharing",
+    essayScenario: "https://drive.google.com/file/d/10McV1FFrD-kFMr09nLnpAXGZY_LiQHyT/view?usp=sharing",
+    ansExplain:
+      '<div class="ExplnAns">Correct answer D.</div>',
+  },
+  {
+  id: 8,
+  q: "Second Scenario: Describe how this business should differentiate between ideal standards and currently attainable standards.",
+  options: [],
+  answer: 4,
+  notID: 2,
+  yourAns: "",
+  yourNote: "",
+  yourChck: "",
+  id2: 1,
+  timeValues: "https://drive.google.com/file/d/1Pub1iP9byprNvqcSSxS_b2L3RSoiD8AN/view?usp=sharing",
+  essayScenario: "https://drive.google.com/file/d/10McV1FFrD-kFMr09nLnpAXGZY_LiQHyT/view?usp=sharing",
+  ansExplain:
+    '<div class="ExplnAns">Correct answer D.</div>',
+  },
+  {
+    id: 9,
+    q: `Second Scenario: Identify and explain two possible reasons why the production process in this scenario regularly fails to meet its maximum quantity.`,
+    options: [],
+    answer: 4,
+    notID: 3,
+    yourAns: "",
+    yourNote: "",
+    yourChck: "",
+    id2: 1,
+    timeValues: "https://drive.google.com/file/d/1Pub1iP9byprNvqcSSxS_b2L3RSoiD8AN/view?usp=sharing",
+    essayScenario: "https://drive.google.com/file/d/10McV1FFrD-kFMr09nLnpAXGZY_LiQHyT/view?usp=sharing",
+    ansExplain:
+      '<div class="ExplnAns">Correct answer D.</div>',
+    },
+    {
+      id: 10,
+      q: "Second Scenario: Identify and explain two benefits of reducing the incidence of stock-outs.",
+      options: [],
+      answer: 4,
+      notID: 4,
+      yourAns: "",
+      yourNote: "",
+      yourChck: "",
+      id2: 1,
+      timeValues: "https://drive.google.com/file/d/1Pub1iP9byprNvqcSSxS_b2L3RSoiD8AN/view?usp=sharing",
+      essayScenario: "https://drive.google.com/file/d/10McV1FFrD-kFMr09nLnpAXGZY_LiQHyT/view?usp=sharing",
+      ansExplain:
+        '<div class="ExplnAns">Correct answer D.</div>',
+    },
+    {
+        id: 11,
+        q: "Second Scenario: Define budgetary slack. Identify and explain two ways this business can reduce the incidence and effect of budgetary slack.",
+        options: [],
+        answer: 4,
+        notID: 5,
+        yourAns: "",
+        yourNote: "",
+        yourChck: "",
+        id2: 1,
+        timeValues: "https://drive.google.com/file/d/1Pub1iP9byprNvqcSSxS_b2L3RSoiD8AN/view?usp=sharing",
+        essayScenario: "https://drive.google.com/file/d/10McV1FFrD-kFMr09nLnpAXGZY_LiQHyT/view?usp=sharing",
+        ansExplain:
+          '<div class="ExplnAns">Correct answer D.</div>',
+    },
+    {
+      id: 12,
+      q: "Second Scenario: Based on information provided, prepare the toy company’s production budget for the month of November. Show your calculations.",
+      options: [],
+      answer: 4,
+      notID: 5,
+      yourAns: "",
+      yourNote: "",
+      yourChck: "",
+      id2: 1,
+      timeValues: "https://drive.google.com/file/d/1Pub1iP9byprNvqcSSxS_b2L3RSoiD8AN/view?usp=sharing",
+      essayScenario: "https://drive.google.com/file/d/10McV1FFrD-kFMr09nLnpAXGZY_LiQHyT/view?usp=sharing",
+      ansExplain:
+        '<div class="ExplnAns">Correct answer D.</div>',
+  }
+
+];
+function updateElementWithArrayCount(array) {
+  var count = array.length;
+
+  // Multiply by 5
+  var result = count * 5;
+
+  if (result > 360) {
+    result = 360;
+  }
+
+  var element = document.getElementById("timeinmins");
+
+  if (element) {
+    element.textContent = result;
+  } else {
+    console.error("Element not found");
+  }
+}
+updateElementWithArrayCount(MainScrptQues);
